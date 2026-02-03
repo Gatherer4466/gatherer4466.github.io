0b389e2717e6cd5c2bc6bd088cec808477b3195a
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-03 12:12:33 +0100

    Create two test views to test routing

---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1 style="background-color: aliceblue">Home</h1>
+    <router-link to="/test">Go to Test</router-link>
+  </div>
+</template>
---
+++ src/views/TestRouting.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1 style="background-color: white">Test</h1>
+
+    <router-link to="/">Back Home</router-link>
+  </div>
+</template>
