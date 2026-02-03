906822054b1fc8748a8c8d3dd3ab59e893e0b527
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-03 21:44:26 +0100

    create ButtonList.vue and import dependencies

---
+++ src/components/ButtonList.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="button-list"></div>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { validRoutedItems } from '@/types/availableRoutes'
+</script>
