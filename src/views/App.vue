3058281a2db35e30281860d0d2137a8404d4d9ff
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-03 12:53:23 +0100

    Create basic navigation component

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,8 +1,10 @@
 <template>
+  <NavigationMenu />
   <router-view />
   <BackgroundCanvas />
 </template>
 
 <script setup lang="ts">
 import BackgroundCanvas from '@/components/BackgroundCanvas.vue'
+import NavigationMenu from '@/components/NavigationMenu.vue'
 </script>
