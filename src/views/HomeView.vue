e7237ba768c8c7aa0da789259c1ef85648bce21f
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-04 15:51:28 +0100

    create temporary padding

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,34 @@
 <template>
-  <div>
-    <h1 style="background-color: aliceblue">Lige nu er der ikke noget for forsiden,
-      <br />
-      klik på en af de der navigations knapper på siden.</h1>
+  <div class="wrapper">
+    <h1 class="message">
+      Lige nu er der ikke noget for forsiden,<br />
+      klik på en af de der navigations knapper på siden.
+    </h1>
+
+    <img
+      class="bottom-image literally just for padding fuck you if youre seing this in inspect element btw"
+      src="https://cdn.7tv.app/emote/01H2EP392G0007NQZJ108V2KRR/4x.png"
+      alt="Emote"
+    />
   </div>
 </template>
+
+<style>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+}
+
+.message {
+  background-color: aliceblue;
+}
+
+.bottom-image {
+  opacity: 0;
+  width: 150px;
+  bottom: 0;
+}
+</style>
