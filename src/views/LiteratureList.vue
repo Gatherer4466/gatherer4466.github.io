970f7700e22bc8ec407eae5d53f0097ec7d29a1e
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-04 12:56:40 +0100

    Make use of the sourcelist compon ent

--- src/views/LiteratureList.vue
+++ src/views/LiteratureList.vue
@@ -1,5 +1,14 @@
 <template>
-  <div>
-    <h1 style="background-color: aliceblue">AaaaaaaaaaaaaaaaaaA</h1>
-  </div>
+ <h2 id="infoTitle" class="titleMargin">
+    <b style="color: slateblue; margin-bottom: 25px">Kildehenvisninger:</b>
+  </h2>
+  <div class="scrollable-content">
+    <SourceList />
+    </div>
+
 </template>
+
+
+<script setup lang="ts">
+import SourceList from '@/components/viewSpecific/literature/SourceList.vue';
+</script>
