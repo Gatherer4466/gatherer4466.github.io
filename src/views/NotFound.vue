9afe729f3fabb975e6a3a3dd4f543f11f48a9867
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-04 14:45:57 +0100

    Add a simple 404 page for non-existant routes

---
+++ src/views/NotFound.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="not-found">
+    <h1>404</h1>
+    <p>Page not found</p>
+    <router-link to="/">Go home</router-link>
+  </div>
+</template>
+
+<style scoped>
+.not-found {
+  text-align: center;
+  color: #41ff41;
+}
+</style>
