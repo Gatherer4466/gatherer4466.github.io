715e4c85c21a068736cc0750251da4b2a5349c97
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-04 10:03:04 +0100

    Move color over to the router's routes instead of having it be in its own file

--- src/factories/router.ts
+++ src/factories/router.ts
@@ -3,15 +3,17 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '@/views/HomeView.vue'
 import Lit from '@/views/LiteratureList.vue'
 
-const routes = [
+export const routes = [
   {
     path: '/',
     name: 'home',
+    color: '#ff4493',
     component: Home,
   },
   {
     path: '/literature',
     name: 'sources',
+    color: '#DCBB00',
     component: Lit,
   },
 ]
--- src/types/availableRoutes.ts
+++
@@ -1,4 +0,0 @@
-export const validRoutedItems = [
-  { name: 'home', color: '#ff1493' },
-  { name: 'sources', color: '#DCBB00' },
-]
