a3d55efabdc1356246c139eb4f5ccfeb808db051
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-04 10:27:38 +0100

    import headercomponent.vue

---
+++ src/components/viewSpecific/HeaderComponent.vue
@@ -0,0 +1,50 @@
+<template>
+  <header class="nav1" id="top">
+    <h1 style="margin-top: 0">
+      <b id="rainbow" :style="{ color: rainbowColor }">Portfølje</b>
+    </h1>
+
+    <p class="smallMargin" id="sub">
+      <b style="color:slateblue">Dette portfølje er til formål for at dokumentere processen fra idé til produkt i løbet af 4. semester.</b>
+    </p>
+  </header>
+</template>
+
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const rainbowColor = ref<string>(getRandomColor())
+
+let rainbowInterval: ReturnType<typeof setInterval> | null = null
+
+onMounted(() => {
+  rainbowInterval = setInterval(() => {
+    rainbowColor.value = getRandomColor()
+  }, 1000)
+
+})
+
+onUnmounted(() => {
+  if (rainbowInterval) clearInterval(rainbowInterval)
+})
+
+function getRandomColor(): string {
+  const charset = '0123456789ABCDEF'
+  let color = '#'
+  for (let i = 0; i < 6; i++) {
+    color += charset[Math.floor(Math.random() * 16)]
+  }
+  return color
+}
+</script>
+
+<style scoped>
+.nav1 {
+  text-align: center;
+}
+
+.smallMargin {
+  margin-top: 0;
+  margin-bottom: 0;
+}
+</style>
