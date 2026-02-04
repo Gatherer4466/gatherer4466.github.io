ccb4956f271507a4627813b785832bae6e7f6c89
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-04 15:34:18 +0100

    Add paths to all the required stuff

--- src/factories/router.ts
+++ src/factories/router.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-
 export const routes = [
   {
     path: '/',
@@ -10,21 +9,55 @@ export const routes = [
     component: () => import('@/views/HomeView.vue'),
   },
   {
+    path: '/produkter',
+    name: 'prod',
+    color: '#4CAF50',
+    inMenu: true,
+    component: () => import('@/views/NotFound.vue'),
+  },
+  {
+    path: '/eksperimenter',
+    name: 'eksper',
+    color: '#FF7043',
+    inMenu: true,
+    component: () => import('@/views/NotFound.vue'),
+  },
+  {
+    path: '/refleksioner',
+    name: 'reflek',
+    color: '#EC407A',
+    inMenu: true,
+    component: () => import('@/views/NotFound.vue'),
+  },
+  {
+    path: '/laering',
+    name: 'planer',
+    color: '#26A69A',
+    inMenu: true,
+    component: () => import('@/views/NotFound.vue'),
+  },
+  {
+    path: '/log',
+    name: 'log',
+    color: '#8D6E63',
+    inMenu: true,
+    component: () => import('@/views/NotFound.vue'),
+  },
+  {
     path: '/literature',
-    name: 'litteratur',
+    name: 'kilder',
     color: '#DCBB00',
     inMenu: true,
     component: () => import('@/views/LiteratureList.vue'),
   },
 
-
-// Handle literally anything that isn't listed here.
+  // Handle literally anything that isn't listed here.
   {
     path: '/:pathMatch(.*)*',
     name: 'NotFound',
     inMenu: false,
-    component: () => import('@/views/NotFound.vue')
-  }
+    component: () => import('@/views/NotFound.vue'),
+  },
 ]
 
 const router = createRouter({
