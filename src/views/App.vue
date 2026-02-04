94a77855574e32177c3f75f5cf30d7042ae7d8db
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-04 10:22:16 +0100

    Refactor app.vue to utilize a table layout

--- src/views/App.vue
+++ src/views/App.vue
@@ -1,10 +1,42 @@
 <template>
-  <ButtonList />
-  <router-view />
-  <BackgroundCanvas />
+  <div id="app">
+    <BackgroundCanvas />
+
+    <main class="content">
+      <table class="mainBox">
+        <tbody>
+          <tr>
+            <th>
+               <HeaderComponent/>
+            </th>
+          </tr>
+
+          <tr>
+            <th>
+              <router-view />
+            </th>
+          </tr>
+        </tbody>
+      </table>
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
 import BackgroundCanvas from '@/components/BackgroundCanvas.vue'
-import ButtonList from '@/components/ButtonList.vue'
+// import ButtonList from '@/components/ButtonList.vue'
+import HeaderComponent from '@/components/viewSpecific/HeaderComponent.vue';
 </script>
+
+<style>
+#app {
+  width: 100%;
+  min-height: 100vh;
+  position: relative;
+}
+
+.content {
+  position: relative;
+  z-index: 1;
+}
+</style>
