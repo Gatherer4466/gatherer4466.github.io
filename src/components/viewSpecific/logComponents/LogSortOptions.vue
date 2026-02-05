6451b28c25c262ac853618f91f92e8081a239500
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-05 09:10:06 +0100

    Create LogSortOptions.vue

---
+++ src/components/viewSpecific/logComponents/LogSortOptions.vue
@@ -0,0 +1,5 @@
+<template>
+<!-- Potentially add filter options for month, and allow to sort based on latest or oldest first. -->
+<!-- ! DEFAULT NEEDS TO BE NEWEST FIRST OF COURSE. -->
+
+</template>
