899ea0aa9ff7299c0ad22110327617f25bff1bf2
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-05 10:40:20 +0100

    Testing

--- src/components/viewSpecific/logComponents/LogSortOptions.vue
+++ src/components/viewSpecific/logComponents/LogSortOptions.vue
@@ -1,5 +1,19 @@
 <template>
 <!-- Potentially add filter options for month, and allow to sort based on latest or oldest first. -->
 <!-- ! DEFAULT NEEDS TO BE NEWEST FIRST OF COURSE. -->
-
+ <a v-for="option in sortOptions" href="#" style="padding: 5px;"> {{ option.name }} </a>
 </template>
+
+
+<script setup lang="ts">
+const sortOptions = [
+  {name: 'newest', default: ''},
+  {name: 'oldest'},
+  {name: 'march'},
+  {name: 'feb'},
+  {name: 'april'},
+  {name: 'may'},
+  {name: 'june'},
+]
+
+</script>
