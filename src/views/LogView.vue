cf67aa930137d3602449c0e08b6db73aa46fbba3
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-05 10:40:46 +0100

    import important components

--- src/views/LogView.vue
+++ src/views/LogView.vue
@@ -1,5 +1,14 @@
 <template>
-
-
+  <!-- <LogSortOptions/> -->
+  <div id="scollable-content">
+    <LogList />
+  </div>
 
 </template>
+
+<script setup lang="ts">
+import LogSortOptions from '@/components/viewSpecific/logComponents/LogSortOptions.vue';
+import LogList from '@/components/viewSpecific/logComponents/LogList.vue';
+
+
+</script>
