762e07d5152c3c2feed796e7720131803ede566a
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-06 08:51:35 +0100

    create LearningView.vue

---
+++ src/views/LearningView.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2>Læringsplaner</h2>
+
+  <LearningFilter
+    @update:category="category = $event"
+    @update:horizon="horizon = $event"
+  />
+
+  <LearningList
+    :category="category"
+    :horizon="horizon"
+  />
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import LearningFilter from '@/components/viewSpecific/learning/GoalSortOptions.vue'
+import LearningList from '@/components/viewSpecific/learning/GoalList.vue'
+
+const category = ref<'all' | 'vue' | 'devops'>('all')
+const horizon = ref<'all' | 'short' | 'mid' | 'long'>('all')
+</script>
