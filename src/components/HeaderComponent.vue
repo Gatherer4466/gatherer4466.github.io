c5145a9fa13e2ad75afa2dd9fe0a0fd739c9142e
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-09 15:53:25 +0100

    Actually remove the hardcoded bit we wanted to get rid of

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -24,7 +24,7 @@ let rainbowInterval: ReturnType<typeof setInterval> | null = null
 onMounted(() => {
   rainbowInterval = setInterval(() => {
     rainbowColor.value = getRandomColor()
-  }, 1000)
+  }, props.interval)
 
 })
 
