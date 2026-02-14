34178814379ea653ad04215e1607096bc6039b52
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-14 17:28:23 +0100

    Utilize said component

--- src/features/literature/LiteratureList.vue
+++ src/features/literature/LiteratureList.vue
@@ -2,6 +2,10 @@
  <h2 id="infoTitle" class="titleMargin">
     <b style="color: slateblue; margin-bottom: 25px">Kildehenvisninger:</b>
   </h2>
+  <SourceSortOptions
+    @update:sort="sort = $event"
+    @update:subject="subject = $event"/>
+
   <div class="scrollable-content">
     <SourceList />
     </div>
@@ -10,5 +14,13 @@
 
 
 <script setup lang="ts">
+// dependencies
+import { ref } from 'vue';
+
+// components
 import SourceList from './components/SourceList.vue';
+import SourceSortOptions from './components/SourceSortOptions.vue';
+
+const sort = ref<'best' | 'worse' | 'id'>('id')
+const subject = ref<'web' | 'devOps' | 'both'>('both')
 </script>
