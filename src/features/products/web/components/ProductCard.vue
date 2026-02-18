58f6f6d4f6011fb05372d373cc39dd5378b4e8a1
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-18 14:37:18 +0100

    Update the design a bit

--- src/features/products/web/components/ProductCard.vue
+++ src/features/products/web/components/ProductCard.vue
@@ -1,37 +1,74 @@
 <template>
-  <div class="card product-card">
+  <div class="card">
     <h3>{{ title }}</h3>
 
-    <p>{{ description }}</p>
 
-    <img v-if="preview" :src="preview" alt="" class="product-preview" />
+      <p v-html="description"></p>
 
-    <a :href="link" target="_blank" rel="noopener">
-      Gå til projekt
-    </a>
+    <div class="projectPreview">
+      <img
+        v-if="preview"
+        :src="preview"
+        :alt="`${title} preview`"
+        :draggable="false"
+      />
+      <p style="font-style: italic">
+        {{ preview ? `` : '[af en eller anden årsag har jeg ikke noget at vise her]' }}
+      </p>
+    </div>
+
+    <div class="subText">
+      <a
+        :href="link"
+        target="_blank"
+        rel="noopener"
+        class="tryButton"
+      >
+        [Prøv produktet her]
+      </a>
+
+      <a
+        v-if="githubUrl"
+        :href="githubUrl"
+        target="_blank"
+        rel="noopener"
+        class="tryButton"
+      >
+        [Se selve koden her]
+      </a>
+
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Product } from '../types'
+import { computed } from 'vue'
 
-defineProps<Product>()
+
+const props = defineProps<Product>()
+
+const githubUrl = computed(() => {
+  if (!props.link) return ''
+
+  try {
+    const url = new URL(props.link)
+    const username = url.hostname.split('.')[0]
+    const repo = url.pathname.replace(/\//g, '')
+
+    return `https://github.com/${username}/${repo}`
+  } catch {
+    return ''
+  }
+})
+
 </script>
 
+
 <style scoped>
-.product-card a {
-  text-decoration: none;
-  display: inline-block;
-  margin-top: 8px;
-}
-
-.product-card a:hover {
-  text-decoration: underline;
-}
-
-.product-gif {
-  width: 100%;
-  margin: 10px 0;
-  border: 1px solid white;
+.projectPreview {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 }
 </style>
