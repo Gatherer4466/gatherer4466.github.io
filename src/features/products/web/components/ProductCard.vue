b26bcfd08e42cd7c53eeb120f5baf63b16109a3e
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-18 10:54:30 +0100

    ...yeah

---
+++ src/features/products/web/components/ProductCard.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card product-card">
+    <h3>{{ title }}</h3>
+
+    <p>{{ description }}</p>
+
+    <img v-if="preview" :src="preview" alt="" class="product-preview" />
+
+    <a :href="link" target="_blank" rel="noopener">
+      Gå til projekt
+    </a>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Product } from '../types'
+
+defineProps<Product>()
+</script>
+
+<style scoped>
+.product-card a {
+  text-decoration: none;
+  display: inline-block;
+  margin-top: 8px;
+}
+
+.product-card a:hover {
+  text-decoration: underline;
+}
+
+.product-gif {
+  width: 100%;
+  margin: 10px 0;
+  border: 1px solid white;
+}
+</style>
