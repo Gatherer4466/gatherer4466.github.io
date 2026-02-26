ff6f7a24e51367f59806a11240573a6bfd34a36d
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-02-26 10:13:16 +0100

    Add readability for where you are

--- src/features/reflection/ReflectionView.vue
+++ src/features/reflection/ReflectionView.vue
@@ -1,4 +1,12 @@
 <template>
+ <h2 id="infoTitle" class="titleMargin">
+    <b style="color: slateblue">Refleksioner for læringsplan:</b>
+  </h2>
+
+  <p style="color: #00ffc8; margin-bottom: 10px">
+    &gt; her er mine refleksioner hver anden uge for mine læringsplaner.
+  </p>
+
   <div class="scrollable-content">
     <ReflectionList />
   </div>
