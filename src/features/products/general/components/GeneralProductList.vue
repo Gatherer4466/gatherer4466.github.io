ed62e70af8d2e6013f5b81bcc286f6e4c6a08dc9
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-03-06 13:42:15 +0100

    Create the list

---
+++ src/features/products/general/components/GeneralProductList.vue
@@ -0,0 +1,21 @@
+<template>
+    <p v-if="products.length === 0" class="about-text">
+      Ingen generelle produkter endnu.
+    </p>
+
+    <div class="scrollable-content">
+      <GeneralProductCard
+        v-for="product in products"
+        :key="product.id"
+        :product="product"
+      />
+    </div>
+</template>
+
+<script setup lang="ts">
+import data from '../data.json'
+import type { GeneralProduct } from '../types'
+import GeneralProductCard from './GeneralProductCard.vue'
+
+const products = data.products as GeneralProduct[]
+</script>
