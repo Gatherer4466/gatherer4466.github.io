50601b57bb3c909c87d016f615247ff468e4fc38
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-05-15 12:24:02 +0200

    dont sort after date

--- src/features/exam/components/ExamInfoList.vue
+++ src/features/exam/components/ExamInfoList.vue
@@ -1,28 +1,17 @@
 <template>
-  <p v-if="products.length === 0" class="about-text">Intet information jeg føler relevant for eksamen endnu</p>
+  <p v-if="products.length === 0" class="about-text">
+    Intet information jeg føler relevant for eksamen endnu
+  </p>
 
   <div class="scrollable-content">
-    <ExamInfoCard v-for="product in sorted" :key="product.id" :product="product" />
+    <ExamInfoCard v-for="product in products" :key="product.id" :product="product" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue'
 import data from '../data.json'
 import type { ExamInformation } from '../types'
 import ExamInfoCard from './ExamInfoCard.vue'
 
-const products = data.products as ExamInformation[]
-
-const sorted = computed(() => {
-  const tempProducts = [...products]
-
-  tempProducts.sort((a, b) => {
-    const aTime = new Date(a.date).getTime()
-    const bTime = new Date(b.date).getTime()
-
-    return bTime - aTime
-  })
-  return tempProducts
-})
+const products = data.ExamInformation as ExamInformation[]
 </script>
