964210f085c248a64fa968fdbf61322bd40c8821
Gatherer4466 <259007195+Gatherer4466@users.noreply.github.com>
2026-05-18 15:08:03 +0200

    Create a block that helps embed youtube links

---
+++ src/features/exam/components/blocks/YoutubeBlock.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="youtube-block">
+    <iframe
+      width="560"
+      height="315"
+      :src="block.embedUrl"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  block: {
+    embedUrl: string
+  }
+}>()
+</script>
